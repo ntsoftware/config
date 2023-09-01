91ca2662e359bb8b019978c540267e0a4b38df3d
Nicolas Torneri <n.torneri@arcds.fr>
2023-09-01 15:21:49 +0200

    allow reinstall on windows

--- install.bat
+++ install.bat
@@ -1,8 +1,4 @@
-@echo off
-
-mklink /J %USERPROFILE%\vimfiles vim
-
-copy gitconfig %USERPROFILE%\.gitconfig
-
-copy aliases.txt %USERPROFILE%
-copy profile.cmd %USERPROFILE%
+if not exist "%USERPROFILE%\vimfiles" mklink /J "%USERPROFILE%\vimfiles" vim
+copy gitconfig "%USERPROFILE%\.gitconfig"
+copy aliases.txt "%USERPROFILE%"
+copy profile.cmd "%USERPROFILE%"
