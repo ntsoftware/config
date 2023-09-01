d77e6da7a999247a35caa4cfd411bd9ed56278cc
Nicolas Torneri <n.torneri@arcds.fr>
2023-09-01 13:07:00 +0000

    allow reinstall on linux

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
-ln -s $PWD/bash_aliases ~/.bash_aliases
-ln -s $PWD/gitconfig ~/.gitconfig
-ln -s $PWD/vim ~/.vim
+ln -sf $PWD/bash_aliases ~/.bash_aliases
+ln -sf $PWD/gitconfig ~/.gitconfig
+ln -sfn $PWD/vim ~/.vim
