bab3e709cbd41dea5f6fdc41edb9a290b66bb9e1
Nicolas Torneri <n.torneri@arcds.fr>
2023-09-11 22:19:18 +0200

    automatic add registry key at windows install

--- install.bat
+++ install.bat
@@ -2,3 +2,4 @@ if not exist "%USERPROFILE%\vimfiles" mklink /J "%USERPROFILE%\vimfiles" vim
 copy gitconfig "%USERPROFILE%\.gitconfig"
 copy aliases.txt "%USERPROFILE%"
 copy profile.cmd "%USERPROFILE%"
+reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "%USERPROFILE%\profile.cmd" /f
--- profile.cmd
+++ profile.cmd
@@ -1,20 +1,8 @@
 @echo off
-
-rem To install, open regedit and add new key
-rem    key: HKEY_CURRENT_USER\SOFTWARE\Microsoft\Command Processor
-rem  value: AutoRun
-rem   data: %USERPROFILE%\profile.cmd
-
 doskey /macrofile=%USERPROFILE%\aliases.txt
 
-rem Chocolatey package manager
-rem https://chocolatey.org/install
-rem
-rem In an administrative command prompt
-rem > choco install -y cmake fzf git ripgrep vim
-
+rem Set default fuzzy finder options
 set FZF_DEFAULT_OPTS=--layout reverse --height 40%%
 
 rem Set default locale
-
-set LC_ALL=C
+set LC_ALL=en_US.UTF-8
